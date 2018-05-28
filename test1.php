f515ab2de5d5aef5e05f9c664ef31cc3b1b9084a
JRevirthu'um <revirthuum@gmail.com>
2018-05-29 00:22:20 +0900

    PPT to Conver from COM dll
    PHP의 모듈을 이용.
    윈도우에서만 동작가능.
    
    @todo 한글파일명도 입력되도록 변경필요 (업로드로 바꾸면 의미 없지만-_-)
    
    CASE 1 은 다른이름으로 저장할때 슬라이드를 이미지로 출력하는 기능
    
    CASE 2 는 내보내기를 통해 슬라이드이미지 용량을 결정하는 것

---
+++ test1.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title>기본 오피스 애플리케이션 이용</title>
+</head>
+<body>
+<?php
+/**
+ * CASE 1 . SaveAs 를 이용한 변환방법
+ * @author beaver <beaver@inforang.com;revirthuum@gmail.com>
+ * @todo 한글로 파일명을 호출 시 안되는 문제 해결 필요.
+ */
+$savePath = realpath(basename(getenv($_SERVER['SCRIPT_NAME'])));
+$dirName = 'ppt_image_'.time();
+$pptFile = 'PPTtest.pptx';
+$ppt = new COM('PowerPoint.Application');
+$ppt->Visible = True;
+$ppt->Presentations->Open(realpath($pptFile));
+$ppt->ActivePresentation->SaveAs("{$savePath}/{$dirName}",17);  //'*18=PNG, 19=BMP*'
+$ppt->Quit;
+$ppt = null;
+?>
+폴더를 생성해서 저장한다 <b><?=$dirName?></b>
+</body>
+</html>
---
+++ test2.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<title>기본 오피스 애플리케이션 이용</title>
+</head>
+<body>
+<?php
+/**
+ * CASE 2 . Export 를 이용한 변환방법
+ * @author beaver <beaver@inforang.com;revirthuum@gmail.com>
+ * @todo 한글로 파일명을 호출 시 안되는 문제 해결 필요.
+ */
+$savePath = realpath(basename(getenv($_SERVER['SCRIPT_NAME'])));
+$dirName = 'ppt_export_'.time();
+mkdir($savePath.'/'.$dirName);
+$pptFile = 'PPTtest.pptx';
+$ppt = new COM('PowerPoint.Application') or die('Unable to instantiate Powerpoint');
+$ppt->Visible = True;
+$data = $ppt->Presentations->Open(realpath($pptFile), false, false, false) or die('Unable to open presentation');
+foreach($data->Slides as $slide):
+    $slideName = 'ppt_'.$slide->SlideNumber;
+    $slide->Export("{$savePath}/{$dirName}/{$slideName}.jpg", 'jpg', 1280, 720);
+endforeach;
+$ppt->Quit;
+$ppt = null;
+?>
+폴더를 생성해서 저장한다 <b><?=$dirName?></b>
+</body>
+</html>
